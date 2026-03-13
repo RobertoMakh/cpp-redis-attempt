a246dab2b026a76a6c22e6c839eaabca6cd18e11
RobertoMakh <robertomachole@gmail.com>
2026-03-14 01:30:29 +0200

    feat: make client interactive to test database memory state

--- client.cpp
+++ client.cpp
@@ -1,30 +1,40 @@
 #include <iostream>
-#include <cstdio>
+#include <string>
 #include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
-int main(){
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: ./client <command> [key] [value]\n";
+        return 1;
+    }
+
+    std::string msg = "";
+    for (int i = 1; i < argc; ++i) {
+        msg += argv[i];
+        if (i < argc - 1) msg += " ";
+    }
+
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0) return 1;
-
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
-    addr.sin_port = ntohs(1234);
-    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
-    int rv = connect(fd,(const struct sockaddr *)&addr, sizeof(addr));
+    addr.sin_port = htons(1234);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    char msg[] = "SET name Roberto";
-    write(fd,msg,strlen(msg));
+    if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        std::cout << "Connection failed!\n";
+        return 1;
+    }
 
-    char rbuf[64] = {};
-    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
+    write(fd, msg.c_str(), msg.length());
 
-    printf("server says: %s\n", rbuf);
+    char rbuf[1024] = {};
+    read(fd, rbuf, sizeof(rbuf) - 1);
+    std::cout << "server says: " << rbuf;
+
     close(fd);
-
     return 0;
 }
